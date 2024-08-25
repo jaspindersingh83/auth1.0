c3d74a9ac4903d4546e47de6712017f0b18a759a
jaspindersingh83 <jaspindersingh83@gmail.com>
2024-08-25 12:47:44 +0530

    Package Updated

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const session = require('express-session');
+const bodyParser = require("body-parser");
+const express = require("express");
+const session = require("express-session");
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -8,9 +8,13 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+  session({
+    saveUninitialized: true,
+    resave: true,
+    secret: "e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re",
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -26,7 +30,7 @@ const sendUserError = (err, res) => {
 // TODO: implement routes
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get("/me", (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
